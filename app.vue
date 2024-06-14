7456c55f07727984bd70eb597d4930786d2987bd
narr07 <dinarpermadi07@gmail.com>
2024-06-15 03:49:19 +0700

    first commit

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <NuxtLoadingIndicator :color="false" class="z-100 bg-primary" />
+  <NuxtPage />
+</template>
+
+<script setup lang="ts">
+const config = useConfig();
+
+useSeoMeta({
+  description: config.value.site.description,
+  ogDescription: config.value.site.description,
+  ogImage: config.value.site.ogImage,
+  twitterCard: 'summary_large_image',
+});
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,58 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    'shadcn-nuxt',
+    '@vueuse/nuxt',
+    '@nuxt/content',
+    'nuxt-icon',
+    '@nuxt/image',
+    '@nuxt/fonts',
+    '@hypernym/nuxt-anime',
+  ],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: 'Ui',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui',
+  },
+  components: {
+    dirs: [
+      {
+        path: './components',
+        ignore: ['**/*.ts'],
+      },
+    ],
+  },
+  content: {
+    documentDriven: true,
+    highlight: {
+      theme: {
+        default: 'github-light',
+        dark: 'github-dark',
+      },
+      preload: ['json', 'js', 'ts', 'html', 'css', 'vue', 'diff', 'shell', 'markdown', 'yaml', 'bash', 'ini'],
+    },
+    navigation: {
+      fields: ['icon'],
+    },
+    experimental: {
+      search: {
+        indexed: true,
+      },
+    },
+  },
+  typescript: {
+    tsConfig: {
+      compilerOptions: {
+        baseUrl: '.',
+      },
+    },
+  },
+});
